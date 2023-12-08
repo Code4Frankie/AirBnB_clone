eb0c6f59a5aab78c2504e25b1d1c1b125752865e
prisca2920 <prisca2920@gmail.com>
2023-12-08 03:53:38 -0800

    classes

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,24 @@
 """creating the base class"""
 import uuid
 import datetime
+import storage
 
 
 class BaseModel:
     """creating an empty class base"""
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         """initializing the class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs:
+            dformat = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, dformat))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        storage.new(self)
 
     def __str__(self):
         """returns the str representation"""
@@ -22,4 +31,8 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dict representation"""
-        for key, value in kwargs.items():
+        dict = self.__dict__.copy()
+        dict["__class__"] = self.__class__.__name__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        return dict
