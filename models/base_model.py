a808f8bafea98931a883029f8fe735f21df1f743
prisca2920 <prisca2920@gmail.com>
2023-12-08 04:46:34 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """creating the base class"""
 import uuid
-import datetime
-import storage
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -10,8 +10,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializing the class"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             dformat = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -23,11 +23,13 @@ class BaseModel:
 
     def __str__(self):
         """returns the str representation"""
+        class_name = self.__class__.__name__
         return f"[{class name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updated the current time"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dict representation"""
